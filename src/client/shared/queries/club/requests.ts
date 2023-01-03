bbf13f5539fb42a5850bd3cbd706ef4d4feeb906
jaepang <jake_king@kakao.com>
2023-01-03 22:36:12 +0900

    event modal improve

--- src/client/shared/queries/club/requests.ts
+++ src/client/shared/queries/club/requests.ts
@@ -1,6 +1,11 @@
-import { NexusGenArgTypes, NexusGenObjects } from '@shared/generated/nexus-typegen'
+import { NexusGenObjects } from '@shared/generated/nexus-typegen'
 import { graphQLClient } from '@client/utils'
-import { CLUBS_QUERY } from './tags'
+import { CLUB_QUERY, CLUBS_QUERY } from './tags'
+
+export const clubQuery = async ({ queryKey }): Promise<{ club: NexusGenObjects['Club'] }> => {
+  const [, variables] = queryKey
+  return await graphQLClient.request(CLUB_QUERY, variables)
+}
 
 export const clubsQuery = async (): Promise<{ clubs: NexusGenObjects['Club'][] }> => {
   return await graphQLClient.request(CLUBS_QUERY)
